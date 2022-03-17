12b1a82b4b6e7fc657a16f2ff3b942dfeda9f0f5
ddaddaradda <jdubird2@gmail.com>
2022-03-17 17:52:40 +0900

    make week1 folder

--- week_1/15650.py
+++ week_1/15650.py
@@ -1,21 +1,22 @@
-import sys
-input = sys.stdin.readline
+###################컴비네이션
+from itertools import combinations
+N, M = map(int,input().split())
+arr=list(range(1,N+1))
+for e in list(combinations(arr, M)):  
+    print(*e)
 
-n, m = map(int, input().split())
 
-s = []
 
-def foo(start):
-  if len(s) == m:
-    print(*s)
-    return
 
-  for i in range(start,n+1):
-    if  i in s:
-      continue
-    
-    s.append(i)
-    foo(i+1)
-    s.pop()
-    
-foo(1)
+###################
+N, M = map(int,input().split())
+s =[] #출력대상
+def dfs(start):
+    if len(s)==M :
+        print(*s)
+        return
+    for i in range(start,N+1):
+        s.append(i)
+        dfs(i+1)
+        s.pop()
+dfs(1)
