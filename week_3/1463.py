43c6139e60e9dfcff79588e52337805b37335f81
MG Park <90205987+Tieck-IT@users.noreply.github.com>
2022-04-02 23:36:56 +0900

    #18 study DP

--- week_3/1463.py
+++ week_3/1463.py
@@ -1,3 +1,24 @@
+# try 4 -> 성공
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+distance = [0] * (n+1)
+
+for i in range(2, n+1):
+  
+  distance[i] = distance[i-1] +1
+  if i%3 ==0:
+    distance[i] = min(distance[i],distance[i//3]+1)
+  if i%2 ==0:
+    distance[i] = min(distance[i],distance[i//2]+1)
+ 
+
+print(distance[n])  
+
+
 # 모범 답안
 n = int(input())
 d = [0] * (n + 1)	## d에 계산된 값을 저장해둔다. n + 1이라고 한 이유는, 1번째 수는 사실 d[1]이 아니고 d[2]이기 때문에, 계산하기 편하게 d[1]을 1번째 인 것 처럼 만들어준다.
